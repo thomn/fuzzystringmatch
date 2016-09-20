f41a0dca3cbdb39fdce6a95b4cb809acfc66be24
seriousManual <seriousstuff@gmx.de>
2016-09-20 08:46:00 +0200

    add example

---
+++ example/example1.js
@@ -0,0 +1,12 @@
+var fuzzyStringmatch = require('../')
+
+var digester = new fuzzyStringmatch.Digester
+var matcher = new fuzzyStringmatch.Matcher(digester)
+
+digester.feed('United States of America')
+digester.feed(new fuzzyStringmatch.Subject('United Kingdom of Great Britain'))
+digester.feed('Germany')
+digester.feed('France')
+digester.feed('Japan')
+
+console.log(matcher.match('Unted'))
